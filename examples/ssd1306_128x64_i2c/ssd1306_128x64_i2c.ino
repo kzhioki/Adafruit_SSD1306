5a0a148e554c4c182e1523194f8dcc9e6c78cdbb
kzhioki <kzhioki@gmail.com>
2019-01-22 00:54:35 +0900

    Update example for Spresense
    - I2C address : 0x3D -> 0x3C
    - Reset pin : none

--- examples/ssd1306_128x64_i2c/ssd1306_128x64_i2c.ino
+++ examples/ssd1306_128x64_i2c/ssd1306_128x64_i2c.ino
@@ -27,7 +27,7 @@
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define OLED_RESET    -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 #define NUMFLAKES     10 // Number of snowflakes in the animation example
@@ -56,7 +56,7 @@ void setup() {
   Serial.begin(9600);
 
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3D)) { // Address 0x3D for 128x64
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x64
     Serial.println(F("SSD1306 allocation failed"));
     for(;;); // Don't proceed, loop forever
   }
